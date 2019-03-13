b87556ddef8dfbe4547f582cbcd236619c29b183
wb-nl429649 <wb-nl429649@alibaba-inc.com>
2019-03-13 16:05:03 +0800

    update

---
+++ server/app.ts
@@ -0,0 +1,30 @@
+import * as Koa from 'koa';
+
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'Hello World';
+});
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  });
+  
+  // x-response-time
+  
+  app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+  });
+  
+  // response
+  
+  app.use(async ctx => {
+    ctx.body = 'Hello World';
+  });
+  
+  app.listen(3000);
+export default app;
